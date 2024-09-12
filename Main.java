b269d77906300aaf80ef5d42859640aab7b862bb
nutsa21 <nutsag@nycstudents.net>
2024-09-12 01:11:45 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 10;
+        double height = 5.6;
+        boolean isStudent = true;
+        String text = "Hello, World!";
+        double total = age + height;
+        age = 11;
+        final int MAX_SCORE = 100;
+        System.out.println(text);
+        System.out.println("Total: " + total);
+        System.out.println("Updated age: " + age);
+        System.out.println("Is student: " + isStudent);
+        System.out.println("Max score: " + MAX_SCORE);
+    }
+}
